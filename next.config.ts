b1a0c749f2b3e9965ba748c8f5cc61713fc9d4f6
anakkaw <103820046+anakkaw@users.noreply.github.com>
2026-02-09 18:50:21 +0700

    Fix basePath for web-app repo

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/project-budget',
+  basePath: '/web-app',
   images: {
     unoptimized: true,
   },
